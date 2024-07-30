5ea1cdc202d70cdc7aa39e606b3e8d363e3df613
Leandro <menoci.leandro@gmail.com>
2024-07-30 20:45:33 -0300

    Scanner

---
+++ src/fundamentos/Console.java
@@ -0,0 +1,26 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Console {
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite o seu nome: ");
+        String nome = entrada.nextLine();
+
+        System.out.print("Digite o seu sobrenome: ");
+        String sobrenome = entrada.nextLine();
+
+        System.out.print("Digite a sua idade: ");
+        int idade = entrada.nextInt();
+
+        // System.out.println("Seu nome é: " + nome + " " + sobrenome + " e sua idadae é: " + idade);
+        System.out.printf("%s %s tem %d anos.", nome, sobrenome, idade);
+
+        entrada.close();
+
+
+    }
+}
