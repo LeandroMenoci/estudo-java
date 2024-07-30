d5803a5bdc66781b97c2c39cb18c6b3b8a66fea9
Leandro <menoci.leandro@gmail.com>
2024-07-30 20:59:14 -0300

    Wrappers

---
+++ src/fundamentos/Wrappers.java
@@ -0,0 +1,30 @@
+package fundamentos;
+
+public class Wrappers {
+    public static void main(String[] args) {
+        
+        // byte
+        Byte b = 100;
+        Short s = 1000;
+        Integer i = 10000;
+        Long l = 100000L;
+
+        System.out.println(b.byteValue());
+        System.out.println(s.toString());
+        System.out.println(i * 3);
+        System.out.println(l / 3);
+
+        Float f = 123.10f;
+        System.out.println(f);
+
+        Double d = 1231.1233;
+        System.out.println(d);
+
+        Boolean bo = Boolean.parseBoolean("true");
+        System.out.println(bo);
+
+        Character c = '#';
+        System.out.println(c + "...");
+
+    }
+}
