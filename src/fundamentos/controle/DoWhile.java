c6aac75f93024e0d122d3b531331fc46fe24e5dc
Leandro <menoci.leandro@gmail.com>
2024-08-02 10:52:50 -0300

    Inciando em repetição

---
+++ src/fundamentos/controle/DoWhile.java
@@ -0,0 +1,23 @@
+package fundamentos.controle;
+
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        String texto = "";
+
+        do {
+            System.out.println("Voce precisa falar as palavras mágicas...");
+            System.out.print("Quer sair? ");
+            texto = entrada.nextLine();
+        } while (!texto.equalsIgnoreCase("sair"));
+
+
+        System.out.println("Obrigado");
+        entrada.close();
+
+    }
+}
---
+++ src/fundamentos/controle/For1.java
@@ -0,0 +1,18 @@
+package fundamentos.controle;
+
+public class For1 {
+    public static void main(String[] args) {
+        
+        // int contador = 1;
+
+        // while (contador <= 10) {
+        //     System.out.println("Bom dia!" + contador);
+        //     contador++;
+        // }
+
+        for (int contador = 0; contador <= 10; contador++) {
+            System.out.println("Bom dia! " + contador);
+        }
+
+    }
+}
