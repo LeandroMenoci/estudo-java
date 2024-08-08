5a19953b677aad75eaa1a4e8cafc8d18db5445a1
Leandro <menoci.leandro@gmail.com>
2024-08-07 22:47:20 -0300

    Set, TreeSet, HashSet

---
+++ src/colecoes/ConjuntoComportado.java
@@ -0,0 +1,32 @@
+package colecoes;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class ConjuntoComportado {
+    public static void main(String[] args) {
+        // Set<String> listaAprovados = new HashSet<String>();
+        Set<String> listaAprovados = new TreeSet<>(); // cria lista ordenada
+        listaAprovados.add("Ana");
+        listaAprovados.add("Carlos");
+        listaAprovados.add("Luca");
+        listaAprovados.add("Pedro");
+
+        for (String candidato : listaAprovados) {
+            System.out.println(candidato);
+        }
+
+
+        Set<Integer> nums = new HashSet<>();
+        nums.add(1);
+        nums.add(7);
+        nums.add(2);
+        nums.add(4);
+
+        for (Integer n : nums) {
+            System.out.println(n);
+        }
+
+    }
+}
