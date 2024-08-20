dea45ddb93822426f052b88e294d2cf33f37f6f8
Leandro <menoci.leandro@gmail.com>
2024-08-20 12:28:19 -0300

    Observer

---
+++ src/padroes/observer/Namorada.java
@@ -0,0 +1,12 @@
+package padroes.observer;
+
+public class Namorada implements ObservadorChegadaAniversariante{
+
+    @Override
+    public void chegou(EventoChegadaAniversariante evento) {
+        System.out.println("Avisar os convidados");
+        System.out.println("Apagas as luzes");
+        System.out.println("Esperar um pouco");
+        System.out.println("Supresa!");
+    }
+}
